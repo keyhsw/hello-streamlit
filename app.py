16b2e2344fe097954b72ccae0b140dfe82ef123b
key <30455795+keyhsw@users.noreply.github.com>
2024-03-15 10:36:39 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+def main():
+    import streamlit as st
+
+    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+
+        **👈 Select a demo from the dropdown on the left** to see some examples
+        of what Streamlit can do!
+
+        ### Want to learn more?
+
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+          forums](https://discuss.streamlit.io)
+    """
+    )
+
+# 运行主函数
+if __name__ == "__main__":
+    main()
